ceb1c9c7677902dbc853744d848033fc6006c30a
Sergey <31565718+SergeyLyashenkoPV191@users.noreply.github.com>
2023-01-09 09:50:12 +0300

    Выполнена Лабораторная работа №4

---
+++ src/main/java/tech/reliab/course/lyashenkosd/bank/utils/UserException.java
@@ -0,0 +1,9 @@
+package tech.reliab.course.lyashenkosd.bank.utils;
+
+public class UserException extends Exception {
+
+    public UserException(String message) {
+        super(message);
+    }
+
+}
